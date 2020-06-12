7b1608fba9556b6c3a9433bf1e5355b196e93726
Pablo <p.herrera.mancera@iescristobaldemonroy.es>
2020-06-12 19:33:00 +0200

    Script con los inserts generado.

---
+++ bd/scriptInserts.sql
@@ -0,0 +1,10 @@
+INSERT INTO `ingrediente` (`nombre`, `precio`, `tipoIngrediente`) VALUES 
+ ('Salsa barbacoa', '0.2', 'SALSA'),
+ ('Salsa yogur', '0.2', 'SALSA'),
+ ('Carne kebab', '1,5', 'CARNE'),
+ ('Ternera', '1', 'CARNE'),
+ ('Champiñones', '1', 'VERDURA'),
+  ('Maíz', '1.2', 'VERDURA'),
+ ('Piña', '0.1', 'FRUTA'),
+ ('Cebolla', '1.2', 'FRUTA')
+ ;
